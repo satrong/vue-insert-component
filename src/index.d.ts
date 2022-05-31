e00796f605f5332cbe3d025ddd8b3732d6456d5c
caohuichang <satrong@foxmail.com>
2022-05-31 17:21:25 +0800

    feat: props type inference

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,19 +1,21 @@
-import type { Component } from 'vue'
+import type { Component, DefineComponent } from 'vue'
+
+type BaseProps = InstanceType<DefineComponent>['$props']
 
 declare function InsertWrap(rootCompoent: Component, containerComponent?: Component): Component
 
 export type Callback = (...args: any[]) => void;
 
-export interface InsertOptions extends Record<string, any> {
-  component: Component;
+export interface InsertOptions<T extends abstract new (...args: any) => any = DefineComponent> extends Record<string, any> {
+  component: T;
   /** transfer to components's `props` */
-  props?: Record<string, any>;
+  props?: Omit<InstanceType<T>['$props'], keyof BaseProps>;
   /** execute after `$uninsert`, `callback`'s arguments come from `$uninsert`'s arguments */
   callback?: Callback;
 }
 
 type uninsertCallback = () => void
-declare function useInsert (options: InsertOptions, containerComponent?: Component): uninsertCallback;
+declare function useInsert<T extends abstract new (...args: any) => any> (options: InsertOptions<T>, containerComponent?: Component): uninsertCallback;
 
 type tryClose = (...args: any[]) => void
 declare function useUninsert (): tryClose;
@@ -24,7 +26,7 @@ export { InsertWrap as default, useInsert, useUninsert, createInsert }
 
 declare module '@vue/runtime-core' {
   export interface ComponentCustomProperties {
-    $insert: (options: InsertOptions, containerComponent?: Component) => uninsertCallback;
+    $insert: <T>(options: InsertOptions<T>, containerComponent?: Component) => uninsertCallback;
     $uninsert(...args: any[]): void;
   }
 }
